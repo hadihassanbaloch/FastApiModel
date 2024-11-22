c6a8767d5e44c30972ccb343a8210b90b2143c45
Hadi Hassan <12hadihassan@gmail.com>
2024-11-22 09:29:54 +0500

    added file structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="HousePrediction",
+    version="0.0.1",
+    author="Hadi",
+    author_email="12hadihassan@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
---
+++ template.py
@@ -0,0 +1,46 @@
+
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "mlProject"
+
+
+list_of_projects = [
+    
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_preprocessing.py",
+    f"{project_name}/components/model_training.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/exceptions/__init__.py",
+    f"{project_name}/pipeline/training.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/prediction.py",
+    "app.py",
+    "requirements.txt",
+    "setup.py"
+]
+
+for filepath in list_of_projects:
+    filepath = Path(filepath)
+    
+    
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
